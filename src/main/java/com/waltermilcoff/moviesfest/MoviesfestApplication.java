1660a97feb6afe854c68a31e35cc7c075289e8fc
waltermilcoff <waltermilcoff@gmail.com>
2022-07-04 10:04:54 -0300

    primer commit del proyecto Movies Fest

---
+++ src/main/java/com/waltermilcoff/moviesfest/MoviesfestApplication.java
@@ -0,0 +1,13 @@
+package com.waltermilcoff.moviesfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesfestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waltermilcoff/moviesfest/MoviesfestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waltermilcoff.moviesfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
